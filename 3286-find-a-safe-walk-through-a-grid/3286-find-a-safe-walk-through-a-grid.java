9e779498c1b77cf644f791157f02874605c3b765
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2024-09-14 13:49:35 -0400

    Time: 29 ms (100%), Space: 45.5 MB (100%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.java
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.java
@@ -1,31 +1,25 @@
 class Solution {
-
     int[][] memo; //store health on a cell
     public boolean findSafeWalk(List<List<Integer>> grid, int health) {
         memo = new int[grid.size()][grid.get(0).size()];
 
-        return dfs(0, 0, grid, new HashSet<>(), health);
+        return dfs(0, 0, grid, health);
     }
 
-    private boolean dfs(int row, int col, List<List<Integer>> grid, Set<Integer> path, int health)
+    private boolean dfs(int row, int col, List<List<Integer>> grid, int health)
     {
-        if (row < 0 || row > grid.size()-1)
+        if (row < 0 || row > grid.size()-1 || col < 0 || col > grid.get(0).size()-1)
         {
             return false;
         }
-        if (col < 0 || col > grid.get(0).size()-1)
+        health -= grid.get(row).get(col);
+
+        if (memo[row][col] >= health )
         {
+            // if we already were here with same or less health, this is not the optimal path
             return false;
         }
-        if (memo[row][col] >= health - grid.get(row).get(col) )
-        {
-            return false;
-        }
-        if (path.contains(row * grid.size() * grid.get(0).size() + col))
-        {
-            return false;
-        }
-        if (health - grid.get(row).get(col) <= 0)
+        if (health == 0)
         {
             return false;
         }
@@ -35,15 +29,13 @@ class Solution {
             return true;
         }
 
-        memo[row][col] = health - grid.get(row).get(col);
-        path.add(row * grid.size() * grid.get(0).size() + col);
+        memo[row][col] = health;
 
-        if (dfs(row+1,col,grid,path, health - grid.get(row).get(col))) return true;
-        if (dfs(row,col+1,grid,path, health - grid.get(row).get(col))) return true;
-        if (dfs(row-1,col,grid,path, health - grid.get(row).get(col))) return true;
-        if (dfs(row,col-1,grid,path, health - grid.get(row).get(col))) return true;
+        if (dfs(row+1,col,grid, health)) return true;
+        if (dfs(row,col+1,grid, health)) return true;
+        if (dfs(row-1,col,grid, health)) return true;
+        if (dfs(row,col-1,grid, health)) return true;
 
-        path.remove(row * grid.size() * grid.get(0).size() + col);
 
         return false;
     }
