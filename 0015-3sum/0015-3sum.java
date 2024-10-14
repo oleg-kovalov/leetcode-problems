ee6e25bd0a40ac4800fd5ee2a137010b4d843fbd
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2024-10-14 20:38:57 +0300

    Time: 1583 ms (5.01%), Space: 54.2 MB (5.19%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
+
+        for (int i=0; i<nums.length; i++)
+        {
+            Set<Integer> matching = new HashSet<>();
+            for (int j=i+1; j<nums.length; j++)
+            {
+                if (matching.contains(nums[j]))
+                {
+                    List<Integer> subResult = new ArrayList<>();
+                    subResult.add(nums[i]);
+                    subResult.add(nums[j]);
+                    subResult.add(-nums[i] - nums[j]);
+                    Collections.sort(subResult);
+                    result.add(subResult);
+                } 
+
+                matching.add(-nums[i] -nums[j]);
+            }
+        }
+
+        return new ArrayList<>(result);
+    }
+}
