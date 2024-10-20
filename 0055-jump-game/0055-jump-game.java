a980c1ac2bc801974180b766e248fbc8dd1de2cc
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2024-10-20 18:06:17 +0300

    Time: 2 ms (79.59%), Space: 46.1 MB (5.97%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,33 +1,17 @@
 class Solution {
     public boolean canJump(int[] nums) {
         if (nums.length == 1) return true;
-
-        Set<Integer> memo = new HashSet<>();
         
-        int targetIdx = nums.length-1;
-        LinkedList<Integer> queue = new LinkedList<>();
-        for (int i = 1; i <= nums[0]; i++)
+        int reachable = nums.length-1;
+        for (int i=nums.length-2; i>=0; i--)
         {
-            if (i == targetIdx) return true;
-
-            queue.add(i);
-            memo.add(i);
-        }
-        while (queue.size() > 0)
-        {
-            int currIdx = queue.poll();
-            for (int i=1; i<=nums[currIdx]; i++)
+            if (i + nums[i] >= reachable)
             {
-                if (currIdx + i == targetIdx) return true;
-
-                if (!memo.contains(currIdx + i))
-                {
-                    memo.add(currIdx + i);
-                    queue.add(currIdx + i);
-                }
+                reachable = i;
             }
         }
 
-        return false;
+        return reachable == 0;
+
     }
 }
