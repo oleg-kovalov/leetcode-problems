ca2a03af5c9f0db0c980e72b0f5f11e218a0d4fd
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2024-10-24 12:27:54 +0300

    Time: 0 ms (100%), Space: 41 MB (93.38%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.java
@@ -21,27 +21,12 @@ class Solution {
     private boolean dfs(TreeNode curr1, TreeNode curr2)
     {
         if (curr1 == null && curr2 == null) return true;
-        if (curr1 == null) return false;
-        if (curr2 == null) return false;
+        if (curr1 == null || curr2 == null) return false;
 
-        int left1 = curr1.left != null ? curr1.left.val : -1;
-        int right1 = curr1.right != null ? curr1.right.val : -1;
-        int left2 = curr2.left != null ? curr2.left.val : -1;
-        int right2 = curr2.right != null ? curr2.right.val : -1;
+        if (curr1.val != curr2.val) return false;
 
-        if (left1 == left2 && right1 == right2)
-        {
-            if (! dfs(curr1.left, curr2.left)) return false;
-            if (! dfs(curr1.right, curr2.right)) return false;
-            return true;
-        } 
-        if (left1 == right2 && right1 == left2)
-        {
-            if (! dfs(curr1.left, curr2.right)) return false;
-            if (! dfs(curr1.right, curr2.left)) return false;
-            return true;
-        }
+        if (dfs(curr1.left, curr2.left) && dfs(curr1.right, curr2.right)) return true;
 
-        return false;
+        return dfs(curr1.left, curr2.right) && dfs(curr1.right, curr2.left);
     }
 }
