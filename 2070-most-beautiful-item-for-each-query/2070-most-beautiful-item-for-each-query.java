fe9849b51646aa3a5cc4c402087d9cc3f96499fe
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2024-11-12 19:31:50 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.java
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.java
@@ -1,28 +1,11 @@
 class Solution {
     public int[] maximumBeauty(int[][] items, int[] queries) {
 
-        Map<Integer,Integer> mbpp = new HashMap<>(); //maxBeautyPerPrice
-        for (int[] item: items)
+        Arrays.sort(items, (a,b) -> a[0] - b[0]);
+        for (int i=1; i<items.length; i++)
         {
-            mbpp.put(item[0], Math.max(item[1], mbpp.getOrDefault(item[0], 0)));
-        }
+            items[i][1] = Math.max(items[i][1], items[i-1][1]);
 
-        List<int[]> maxBeauty = new ArrayList<>();
-        for (Map.Entry<Integer, Integer> entry: mbpp.entrySet())
-        {
-            maxBeauty.add(new int[]{entry.getKey(), entry.getValue()});
-        }
-
-        Collections.sort(maxBeauty, (a, b) -> a[0] - b[0]);
-
-        for (int i=1; i<maxBeauty.size(); i++)
-        {
-            int[] pair = maxBeauty.get(i);
-            int price = pair[0];
-            int beauty = pair[1];
-            int prevBeauty = maxBeauty.get(i-1)[1];
-
-            maxBeauty.set(i, new int[]{price, Math.max(beauty, prevBeauty)});
         }
 
         int[] result = new int[queries.length];
@@ -32,41 +15,23 @@ class Solution {
             int target = queries[i];
 
             int lo = 0;
-            int hi = maxBeauty.size()-1;
-            boolean found = false;
-            while (lo < hi)
+            int hi = items.length-1;
+            int maxBeauty = 0;
+            while (lo <= hi)
             {
                 int mid = (lo + hi) / 2;
-                int midPrice = maxBeauty.get(mid)[0];
 
-                if (midPrice == target)
-                {
-                    result[i] = maxBeauty.get(mid)[1];
-                    found = true;
-                    break;
-                }
-                else if (midPrice > target)
+                if (items[mid][0] > target)
                 {
                     hi = mid-1;
                 } else {
+                    // found matching price, continue search if better is possible 
+                    maxBeauty = Math.max(maxBeauty, items[lo][1]);
                     lo = mid + 1;
                 }
             }
 
-            if (!found) {
-                if (target >= maxBeauty.get(lo)[0])
-                {
-                    result[i] = maxBeauty.get(lo)[1];
-                } else {
-                    if (lo > 0)
-                    {
-                        result[i] = maxBeauty.get(lo-1)[1];
-                    } else {
-                        result[i] = 0;
-                    }
-                }
-            }
-
+            result[i] = maxBeauty;
 
         }
 
