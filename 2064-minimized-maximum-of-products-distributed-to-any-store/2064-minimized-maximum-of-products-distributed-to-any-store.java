6dc056fe61e8b42e52e7dc048e74905b3737d7a3
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2024-11-14 18:36:39 +0200

    Time: 1633 ms (10%), Space: 57 MB (37.7%) - LeetHub

--- 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.java
+++ 2064-minimized-maximum-of-products-distributed-to-any-store/2064-minimized-maximum-of-products-distributed-to-any-store.java
@@ -1,30 +1,22 @@
 class Solution {
     public int minimizedMaximum(int n, int[] quantities) {
 
-        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> roundUp(b[0], b[1]) - roundUp(a[0], a[1]));
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> b[2] - a[2]);
         
         for (int m=0; m < quantities.length; m++)
         {
-            maxHeap.offer(new int[]{quantities[m], 1});
+            maxHeap.offer(new int[]{quantities[m], 1, quantities[m]});
         }
 
         for (int i=1; i<= n-quantities.length; i++)
         {
             int[] curr = maxHeap.poll();
             curr[1] += 1;
+            curr[2] = roundUp(curr[0], curr[1]);
             maxHeap.offer(curr);
         }
 
-        // int max = 0;
-        // while (maxHeap.size() > 0)
-        // {
-        //     int[] curr = maxHeap.poll();
-        //     max = Math.max(max, roundUp(curr[0], curr[1]));
-        // }
-
-        // return max;
-
-        return roundUp(maxHeap.peek()[0], maxHeap.peek()[1]);
+        return maxHeap.peek()[2];
     }
 
     private int roundUp(int a, int b)
