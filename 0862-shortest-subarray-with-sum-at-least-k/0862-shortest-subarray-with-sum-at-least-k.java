14d7e12ab78abc9d760b7a7c25a42e2a852c9d73
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2024-11-18 18:03:24 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.java
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.java
@@ -1,26 +1,64 @@
 class Solution {
-    public int shortestSubarray(int[] nums, int k) {
-
-
-        int result = Integer.MAX_VALUE;
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        int rollingSum = 0;
-
-        for (int i=0; i<nums.length; i++)
+    
+        public int shortestSubarray(int[] nums, int k)
         {
-            rollingSum += nums[i];
-            if (rollingSum >= k) result = Math.min(result, i+1);
-            
-            while (minHeap.peek() != null && rollingSum - minHeap.peek()[0] >= k)
+            if (nums.length == 1) return nums[0] <= k ? 1 : 0;
+
+            ArrayList<int[]> stack = new ArrayList<>();
+            int minLen = Integer.MAX_VALUE;
+
+            int[] prefix = new int[nums.length];
+            prefix[0] = nums[0];
+
+            if (prefix[0] > k) return 1;
+
+            stack.add(new int[]{prefix[0], 0});
+            for (int i=1; i<nums.length; i++)
             {
-                result = Math.min(result, i - minHeap.poll()[1]);
+                prefix[i] = prefix[i-1] + nums[i];
+
+                while (stack.size() > 0 && stack.get(stack.size()-1)[0] >= prefix[i] )
+                {
+                    stack.remove(stack.size()-1);
+                }
+
+                stack.add(new int[]{prefix[i], i});
+
+                int discard = rightmostDiscard(stack,  prefix[i] - k);
+                if (discard == -1 && prefix[i] >= k)
+                {
+                    minLen = Math.min(minLen, i + 1); // length from 0 to i
+                }
+                else if (discard != -1)
+                {
+                    minLen = Math.min(minLen, i - stack.get(discard)[1]); // length from discard to i
+                }
+
             }
 
-            minHeap.offer(new int[] {rollingSum, i});
+            return minLen == Integer.MAX_VALUE ? -1 : minLen;
 
         }
 
-        return result == Integer.MAX_VALUE ? -1 : result;
+        private int rightmostDiscard(ArrayList<int[]> stack, int target)
+        {
+            // find the rightmost element that is  <= target
+            int lo = 0;
+            int hi = stack.size() -1;
+            int result = -1;
+            while (lo <= hi)
+            {
+                int mid = ( lo + hi) / 2;
+                if (stack.get(mid)[0] <= target)
+                {
+                    result = mid;
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
 
-    }
+            return result;
+        }
+
 }
