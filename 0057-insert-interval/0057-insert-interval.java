2a94199b08e6912129dd1da57955ec709691a71f
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2024-11-22 15:07:28 +0200

    Time: 11 ms (5.59%), Space: 46.5 MB (5.62%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,48 +1,38 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> intervalsWOveralap = new ArrayList<>();
+        if (intervals.length == 0) return new int[][]{newInterval};
 
-        // insert new interval according to ordering by start, intervals may overlap
-        int newIdx = 0;
-        for (int i=0; i<intervals.length; i++)
+        Map<Integer, Integer> line = new TreeMap<>();
+
+        for (int[] interval : intervals)
         {
-            if (intervals[i][0] < newInterval[0])
-            {
-                intervalsWOveralap.add(intervals[i]);
-                newIdx++;
-            } else {
-                break;
-            }
+            line.put(interval[0], line.getOrDefault(interval[0], 0) + 1);
+            line.put(interval[1], line.getOrDefault(interval[1], 0) - 1);
         }
-        intervalsWOveralap.add(newInterval);
-        for (int i=newIdx; i < intervals.length; i++) {
-            intervalsWOveralap.add(intervals[i]);
-        }
+        line.put(newInterval[0], line.getOrDefault(newInterval[0], 0) + 1);
+        line.put(newInterval[1], line.getOrDefault(newInterval[1], 0) - 1);
 
         List<int[]> result = new ArrayList<>();
-        for (int i=0; i<intervalsWOveralap.size(); i++)
+        int runningCount = 0;
+        int start = -1;
+        for (Map.Entry<Integer, Integer> entry: line.entrySet())
         {
-            int[] currentInterval = { intervalsWOveralap.get(i)[0], intervalsWOveralap.get(i)[1] };
-            while (i < intervalsWOveralap.size() && overlap(currentInterval, intervalsWOveralap.get(i)))
+            // interval can be zero length
+            if (runningCount == 0)
             {
-                currentInterval = merge(currentInterval, intervalsWOveralap.get(i));
-                i++;
+                start = entry.getKey();
             }
-            i--;
-            result.add(currentInterval);
+
+            runningCount += entry.getValue();
+            if (runningCount == 0)
+            {
+                result.add(new int[] {start, entry.getKey()});
+            }
         }
 
 
         return result.toArray(new int[result.size()][2]);
-    }
-
-    private boolean overlap (int[] one, int[] two)
-    {
-        return Math.max(one[0], two[0]) <= Math.min(one[1], two[1]);
-    }
-
-    private int[] merge (int [] one, int[] two)
-    {
-        return new int[] { Math.min(one[0], two[0]), Math.max(one[1], two[1]) };
+    
+    
     }
 }
