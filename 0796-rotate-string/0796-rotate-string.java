e90f588033241b9197373d69c643efa734fa7fc6
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2024-11-30 13:39:36 +0200

    Time: 1 ms (19.74%), Space: 41.5 MB (29.09%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,65 +1,55 @@
 class Solution {
-        public boolean rotateString(String s, String goal)
-        {
-            if (s.length() != goal.length()) return false;
-
-            String doubleStr = s + s;
-
-            return kmpSearch(doubleStr, goal);
-
-        }
-
-        private boolean kmpSearch(String text, String pattern)
-        {
-            int[] lps = computeLPS(pattern);
-            int textIndex = 0;
-            int patternIndex = 0;
-
-            while (textIndex < text.length())
-            {
-                if (text.charAt(textIndex) == pattern.charAt(patternIndex))
-                {
-                    textIndex++;
-                    patternIndex++;
-                    if (patternIndex == pattern.length()) return true;
-                }
-                else if (patternIndex > 0)
-                {
-                    patternIndex = lps[patternIndex - 1];
-                }
-                else {
-                    textIndex++;
-                }
-            }
-
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length())
             return false;
+
+        String doubleStr = s + s;
+
+        return kmpSearch(doubleStr, goal);
+
+    }
+
+    private boolean kmpSearch(String text, String pattern) {
+        int[] lps = computeLPS(pattern);
+        int textIndex = 0;
+        int patternIndex = 0;
+
+        while (textIndex < text.length()) {
+            if (text.charAt(textIndex) == pattern.charAt(patternIndex)) {
+                textIndex++;
+                patternIndex++;
+                if (patternIndex == pattern.length())
+                    return true;
+            } else if (patternIndex > 0) {
+                patternIndex = lps[patternIndex - 1];
+            } else {
+                textIndex++;
+            }
         }
 
-        private int[] computeLPS(String pattern)
-        {
-            int[] lps = new int[pattern.length()];
-            int length = 0;
-            int index = 1;
+        return false;
+    }
 
-            while (index < pattern.length())
-            {
-                if (pattern.charAt(index) == pattern.charAt(length))
-                {
-                    length++;
-                    lps[index] = length;
+    private int[] computeLPS(String pattern) {
+        int[] lps = new int[pattern.length()];
+        int length = 0;
+        int index = 1;
+
+        while (index < pattern.length()) {
+            if (pattern.charAt(index) == pattern.charAt(length)) {
+                length++;
+                lps[index] = length;
+                index++;
+            } else {
+                if (length > 0) {
+                    length = lps[length - 1];
+                } else {
+                    lps[index] = 0;
                     index++;
                 }
-                else {
-                    if (length > 0)
-                    {
-                        length = lps[length - 1];
-                    } else {
-                        lps[index] = 0;
-                        index++;
-                    }
-                }
             }
-
-            return lps;
         }
+
+        return lps;
+    }
 }
