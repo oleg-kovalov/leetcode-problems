71bf7a0fa6ed679a9234bda3f391af45abe9364f
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2024-12-10 19:57:09 +0200

    Time: 4 ms (79.65%), Space: 43.1 MB (85.5%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.java
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.java
@@ -1,25 +1,53 @@
 class Solution {
     public int maximumLength(String s) {
         
-        int max = -1;
-        for (int start=0; start < s.length() - 2; start++)
+        HashMap<Character, List<Integer>> substrings = new HashMap<>();
+
+        int count = 1;
+        char prev = s.charAt(0);
+        for (int i=1; i< s.length(); i++)
         {
-            for (int end=start + 1; end <= s.length(); end++)
+            char curr = s.charAt(i);
+            if (curr == prev)
             {
-                if (end > start + 1 && s.charAt(end-1) != s.charAt(end-2)) break;
+                count += 1;
+            } else {
+                List<Integer> list = substrings.getOrDefault(prev, new ArrayList<>());
+                list.add(count);
+                substrings.put(prev, list);
 
-                String substr = s.substring(start, end);
-                int secondMatch = s.indexOf(substr, start + 1);
-                if (secondMatch == -1) break;
-                int thirdMatch = s.indexOf(substr, secondMatch + 1);
-                if (thirdMatch == -1) break;
-
-                max = Math.max(max, substr.length());
-                // System.out.println("found " + substr);
+                count = 1;
+                prev = curr;
             }
         }
 
-        return max;
+        List<Integer> list = substrings.getOrDefault(prev, new ArrayList<>());
+        list.add(count);
+        substrings.put(prev, list);
 
+        int result = 0;
+        for (Map.Entry<Character,List<Integer>> entry : substrings.entrySet())
+        {
+            int maxForChar = 0;
+            List<Integer> lengthes = entry.getValue();
+            Collections.sort(lengthes, (a,b) -> b - a);
+
+            maxForChar = Math.max(maxForChar, lengthes.get(0) - 2);
+
+            if (lengthes.size() >= 2)
+            {
+                maxForChar = Math.max(maxForChar, Math.min(lengthes.get(0) - 1, lengthes.get(1)));
+            }
+
+            if (lengthes.size() >= 3)
+            {
+                maxForChar = Math.max(maxForChar, lengthes.get(2));
+            }
+
+            result = Math.max(result, maxForChar);
+        
+        }
+
+        return result == 0 ? -1 : result;
     }
 }
