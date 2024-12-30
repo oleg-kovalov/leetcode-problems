d7259bf83b755c0aa8c71f450c65adf2411ae3ed
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2024-12-30 20:39:09 +0200

    Time: 47 ms (5.37%), Space: 78.7 MB (5.37%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.java
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.java
@@ -3,7 +3,7 @@ class Solution {
     int high;
     int zero;
     int one;
-    HashMap<String, Long> memo;
+    HashMap<Integer, Long> memo;
     public int countGoodStrings(int low, int high, int zero, int one) {
         this.low = low;
         this.high = high;
@@ -19,14 +19,14 @@ class Solution {
     private long backtracking(StringBuilder str, int length)
     {
         if (length > high) return 0;
-        if (memo.containsKey(str.toString())) return memo.get(str.toString());
+        if (memo.containsKey(high - length)) return memo.get(high - length);
 
         long count = (length >= low ? 1 : 0) 
             + backtracking(str.append('0'), length + zero)
             + backtracking(str.append('1'), length + one);
         
-        memo.put(str.toString(), count);
-        return count;
+        memo.put(high - length, count);
+        return count % 1_000_000_007;
     }
     
 }
