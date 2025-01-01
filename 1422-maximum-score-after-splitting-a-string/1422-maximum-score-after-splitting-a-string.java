5d9f7175aa76597025d8c5d56083c138c479d723
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-01-01 13:00:35 +0200

    Time: 2 ms (39.66%), Space: 41.6 MB (49.68%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
@@ -1,28 +1,26 @@
 class Solution {
     public int maxScore(String s) {
-        // result = leftZero + rightOnes 
-        // => leftZero - leftOnes + totalOnes 
-        // => bestLeft + totalOnes
-
-        int leftZero = 0;
-        int leftOnes = 0;
-        int bestLeft = Integer.MIN_VALUE;
-
-        for (int i=0; i<s.length()-1; i++)
+        int[] zeros = new int[s.length()];
+        zeros[0] = (s.charAt(0) == '0' ? 1 : 0);
+        for (int i=1; i<s.length(); i++)
         {
-            if (s.charAt(i) - '1' == 0)
-            {
-                leftOnes++;
-            } else {
-                leftZero++;
-            }
-
-            bestLeft = Math.max(bestLeft, leftZero - leftOnes);
+            zeros[i] = zeros[i-1] + (s.charAt(i) == '0' ? 1 : 0);
         }
 
-        int totalOnes = leftOnes + (s.charAt(s.length()-1) - '1' == 0 ? 1 : 0);
+        int[] ones = new int[s.length()];
+        ones[s.length() - 1] = (s.charAt(s.length() - 1) == '1' ? 1 : 0);
+        for (int i=s.length() - 2; i >=0; i--)
+        {
+            ones[i] = ones[i+1] + (s.charAt(i) == '1' ? 1 : 0);
+        }
 
-        return bestLeft  + totalOnes ;
+        int maxScore = -1;
+        for (int i=1; i<s.length(); i++)
+        {
+            maxScore = Math.max(maxScore, zeros[i-1] + ones[i]);
+        }
+
+        return maxScore;
 
     }
 }
