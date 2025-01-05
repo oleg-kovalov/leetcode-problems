1657dfdeb8a408b1efb24b71909674d22e6b6e50
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-01-05 22:35:57 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String shiftingLetters(String s, int[][] shifts) {
+        int[] combined = new int[s.length()];
+
+        for (int[] shift : shifts)
+        {
+            int direction = shift[2] == 1 ? 1 : -1;
+            combined[shift[0]] += direction;
+            if (shift[1] + 1 < s.length()) combined[shift[1] + 1] -= direction;
+        }
+
+        StringBuilder result = new StringBuilder();
+        int runningSum = 0;
+        for (int i=0; i<s.length(); i++)
+        {
+            runningSum += combined[i];
+            // System.out.println(runningSum);
+            int shift = (s.charAt(i) - 'a' + 26 + runningSum) % 26;
+            System.out.println(shift);
+            result.append((char)('a' + shift));
+        }
+
+        return result.toString();
+
+
+    }
+}
