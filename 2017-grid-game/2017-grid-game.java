5a113c5564b113c964c0cbb2ac492c395378f016
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-01-21 22:53:59 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2017-grid-game/2017-grid-game.java
@@ -0,0 +1,31 @@
+class Solution {
+    public long gridGame(int[][] grid) {
+        int n = grid[0].length;
+
+        long[] upper = new long[n];
+        upper[n-1] = grid[0][n-1];
+        for (int i=n-2; i>=0; i--)
+        {
+            upper[i] = upper[i+1] + grid[0][i];
+        }
+
+        long[] lower = new long[n];
+        lower[0] = grid[1][0];
+        for (int i=1; i<n; i++)
+        {
+            lower[i] = lower[i-1] + grid[1][i];
+        }
+        System.out.println(Arrays.toString(upper));
+        System.out.println(Arrays.toString(lower));
+
+        long minScore = Math.min(upper[1], lower[n-2]);
+        for (int i=1; i<n-1; i++)
+        {
+            minScore = Math.min(minScore, Math.max(upper[i+1], lower[i-1]));
+        }
+
+        return minScore;
+
+
+    }
+}
