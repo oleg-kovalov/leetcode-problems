23dfda4fa256c31670483824a7f2adfde33a48e3
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-03-24 20:39:31 +0100

    Time: 1 ms (92.95%), Space: 45 MB (6.13%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,30 +1,31 @@
 class Solution {
-    private List<List<Integer>> result = new ArrayList<>();
+//[[]]
+//[[1]]
+//[[2,1], [1,2]]
+//[[3,2,1],[2,3,1],[2,1,3],  [3,1,2],[1,3,2],[1,2,3]]
+
 
     public List<List<Integer>> permute(int[] nums) {
-        permuteWithBacktracking(new LinkedList<>(), nums);
+        List<List<Integer>> totalLists = new ArrayList<>();
+        totalLists.add(new ArrayList<>());
 
-        return result;
-    }
-
-    private void permuteWithBacktracking(LinkedList<Integer> permutation, int[] nums)
-    {
         for (int i=0; i<nums.length; i++)
         {
-            final int num = nums[i];
-            if (! permutation.contains(num))
+            List<List<Integer>> currLists = new ArrayList<>();
+            int num = nums[i];
+            for (List<Integer> list: totalLists)
             {
-                permutation.add(num);
-                permuteWithBacktracking(permutation, nums);
+                for (int j=0; j<list.size() + 1; j++)
+                {
+                    List<Integer> newList = new ArrayList<>(list);
+                    newList.add(j, num);
+                    currLists.add(newList); 
+                }
             }
+            totalLists = currLists; 
         }
 
-        if (permutation.size() == nums.length)
-        {
-            // System.out.println(permutation);
-            result.add(new ArrayList<>(permutation));
-        }
-
-        if (! permutation.isEmpty()) permutation.removeLast();
+        return totalLists;
     }
+
 }
