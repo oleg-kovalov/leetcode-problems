74fbdbc4d0c1f7096fa0f67c385f2b581945681e
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-03-28 10:47:35 +0100

    Time: 281 ms (23.78%), Space: 53.4 MB (9.59%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,32 +1,36 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> result = new HashSet<>();
-        Set<Integer> starts = new HashSet<>();
+        Arrays.sort(nums);
+
+        List<List<Integer>> result = new ArrayList<>();
+        HashSet<Integer> thirdSet = new HashSet<>();
 
         for (int i=0; i<nums.length; i++)
         {
-            if (starts.contains(nums[i]))
-            {
-                continue;
-            }
-            starts.add(nums[i]);
-            Set<Integer> matching = new HashSet<>();
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            int first = nums[i];
+            thirdSet.clear();
             for (int j=i+1; j<nums.length; j++)
             {
-                if (matching.contains(nums[j]))
+                int curr = nums[j]; //second or third
+                if (thirdSet.contains(curr))
                 {
-                    List<Integer> subResult = new ArrayList<>();
-                    subResult.add(nums[i]);
-                    subResult.add(nums[j]);
-                    subResult.add(-nums[i] - nums[j]);
-                    Collections.sort(subResult);
-                    result.add(subResult);
-                } 
+                    // third
+                    List<Integer> triplet = new ArrayList<>();
+                    triplet.add(first);
+                    triplet.add(0 - first - curr);
+                    triplet.add(curr);
+                    result.add(triplet);
 
-                matching.add(-nums[i] -nums[j]);
+                    thirdSet.remove(curr);
+                } else {
+                    //second
+                    if (j > i+1 && curr == nums[j-1]) continue;
+                    thirdSet.add( 0 - first - curr);
+                }
             }
         }
 
-        return new ArrayList<>(result);
+        return result;
     }
 }
