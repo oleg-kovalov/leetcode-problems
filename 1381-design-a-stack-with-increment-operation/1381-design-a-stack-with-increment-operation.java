ea9cb9c17c5ae879f18dc0a98fea14c863140884
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-04-04 19:30:23 +0200

    Time: 4 ms (100%), Space: 45.6 MB (71.33%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.java
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.java
@@ -1,36 +1,46 @@
-    class CustomStack {
+class CustomStack {
 
-        int[] array;
-        int pointer;
-        
-        public CustomStack(int maxSize) {
-            array = new int[maxSize];
-            Arrays.fill(array, -1);
-            pointer = -1;
-        }
+    private int[] arr;
+    private int[] incr;
+    private int index;
 
-        public void push(int x) {
-            if (pointer == array.length-1) return;
-            
-            pointer++;
-            array[pointer] = x;
-        }
 
-        public int pop() {
-            if (pointer == -1) return -1;
-            pointer--;
-            return array[pointer+1];
-        }
-
-        public void increment(int k, int val) {
-            if (pointer == -1) return;
-            
-            for (int i=0; i <= Math.min(pointer, k-1); i++)
-            {
-                array[i] += val;
-            }
-        }
+    public CustomStack(int maxSize) {
+        arr = new int[maxSize];
+        incr = new int[maxSize];
+        index = 0;
     }
+    
+    public void push(int x) {
+        if (index == arr.length) return;
+
+        arr[index] = x;
+        index += 1;
+    }
+
+
+    //"CustomStack","pop","pop","pop","pop","pop","pop","pop","pop","increment","push","increment","pop","pop","increment","pop","pop","increment","increment","pop","pop","pop","increment","pop","increment","increment","pop","increment","push","pop"
+
+    public int pop() {
+        if (index == 0) return -1;
+
+        index -= 1;
+        int inc = incr[index];
+        incr[index] = 0;
+        if (index > 0)
+        {
+            incr[index - 1] += inc;
+        }
+
+        return arr[index] + inc;
+    }
+    
+    public void increment(int k, int val) {
+        if (index == 0) return;
+
+        incr[Math.min(k, index) - 1] += val;
+    }
+}
 
 /**
  * Your CustomStack object will be instantiated and called as such:
