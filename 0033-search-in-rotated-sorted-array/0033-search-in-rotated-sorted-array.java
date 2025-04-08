bebe6f0209de918f77bac1ff3f440c0bc3d5bb00
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-04-08 12:19:17 +0200

    Time: 0 ms (100%), Space: 42.1 MB (52.08%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,69 +1,41 @@
 class Solution {
     public int search(int[] nums, int target) {
+
         if (nums.length == 1) return nums[0] == target ? 0 : -1;
 
-        int maxId = findMaxId(nums);
-        if (maxId == -1) return -1; //should never happen
-
-        if (nums[0] <= target && target <= nums[maxId])
-        {
-            return search(nums, target, 0, maxId);
-        } else if (maxId < nums.length - 1 && nums[maxId+1] <= target && target <= nums[nums.length - 1])
-        {
-            return search(nums, target, maxId+1, nums.length - 1);
-        } else 
-        {
-            return -1;
-        }
-    }
-
-    private int search(int[] nums, int target, int lo, int hi)
-    {
-        while (lo <= hi)
-        {
-            int mid = lo + (hi - lo) / 2;
-
-            if (nums[mid] < target)
-            {
-                lo = mid + 1;
-            } else if (nums[mid] > target)
-            {
-                hi = mid - 1;
-            } else {
-                return mid;
-            }
-        }
-
-        return -1;
-    }
-
-    private int findMaxId(int[] nums)
-    {
-        if (nums[0] < nums[nums.length - 1]) return nums.length - 1;
-
-        int first = nums[0];
         int lo = 0;
-        int hi = nums.length - 2;
+        int hi = nums.length - 1;
 
         while (lo <= hi)
         {
             int mid = lo + (hi - lo) / 2;
-            
-            if (nums[mid] > nums[mid + 1])
-            {
-                return mid;
-            }
 
-            if (nums[mid] >= first)
+            if (nums[mid] == target) return mid;
+
+            if (nums[mid] >= nums[0]) // left part is normal sorted
             {
-                lo = mid + 1;
-            } else if (nums[mid] < first) {
-                hi = mid - 1;
+                if (nums[0] <= target && target < nums[mid])
+                {
+                    hi = mid - 1;
+                } else
+                {
+                    lo = mid + 1;
+                }
+            } else // right part is normal sorted
+            {
+                if (nums[mid] < target && target <= nums[nums.length - 1])
+                {
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }           
             }
         }
 
         return -1;
 
+
     }
+
 
 }
