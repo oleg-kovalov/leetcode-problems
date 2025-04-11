ad8c76c5c9e8bbb4a6f232e4818f05a4694140f1
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-04-11 19:32:13 +0200

    Time: 7 ms (83.99%), Space: 45.8 MB (9.01%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.java
@@ -4,27 +4,23 @@ class Solution {
             // [2,2,4,5,6]   ,2
             // [1,1,3,4,5] , 2
 
-            HashSet<String> set = new HashSet<>();
+            int count = 0;
             Arrays.sort(nums);
 
             for (int i=0; i<nums.length; i++)
             {
+                if (i > 0 && nums[i] == nums[i-1]) continue;
+
                 int num = nums[i];
-                // int lower = search(nums, i+1, nums.length -1, num - k);
-                // if (lower >= 0)
-                // {
-                //     set.add(nums[lower] + "-" + num);
-                // }
 
                 int upper = search(nums, i+1, nums.length - 1, num + k);
                 if (upper >= 0)
                 {
-                    set.add(num + "-" + nums[upper]);
+                    count += 1;
                 }
             }
 
-            // System.out.println(set);
-            return set.size();
+            return count;
 
     }
 
