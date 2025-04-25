5443ab61dd3fb0ac8f15ecb3d92a9c53179fe667
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-04-25 19:35:31 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+
+//    1 - 4 - 2 - 3
+//            ^   ^
+
+
+//     1 - 5 - 2 - 3 - 4
+//                 ^   ^
+//     4  
+//     3
+//     2
+//     5
+//     1 
+//     
+
+
+class Solution {
+    public void reorderList(ListNode head) {
+        return;
+    }
+}
