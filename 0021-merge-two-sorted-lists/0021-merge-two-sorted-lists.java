212b151b3d42e2fa66b2a23019d4b3a5680f2f5a
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-04-26 13:57:57 +0200

    Time: 1 ms (3.67%), Space: 42.4 MB (74.65%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -9,61 +9,35 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode listNode1, ListNode listNode2)
-    {
-        final ListNode resHead;
-        if (listNode1 == null && listNode2 == null)
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode fakeHead = new ListNode();
+        ListNode curr = fakeHead;
+
+        while (list1 != null || list2 != null)
         {
-            return null;
-        }
-        else if (listNode1 == null)
-        {
-            return listNode2;
-        }
-        else if (listNode2 == null)
-        {
-            return listNode1;
-        }
-        else if (listNode1.val < listNode2.val)
-        {
-            resHead = new ListNode(listNode1.val);
-            mergeNode(listNode1.next, listNode2, resHead);
-        }
-        else {
-            resHead = new ListNode(listNode2.val);
-            mergeNode(listNode1, listNode2.next, resHead);
+            if (list1 == null)
+            {
+                curr.next = new ListNode(list2.val);
+                list2 = list2.next;
+            } else if (list2 == null)
+            {
+                curr.next = new ListNode(list1.val);
+                list1 = list1.next;
+            } else {
+                if (list1.val <= list2.val)
+                {
+                    curr.next = new ListNode(list1.val);
+                    list1 = list1.next;
+                } else {
+                    curr.next = new ListNode(list2.val);
+                    list2 = list2.next;
+                }
+            }
+
+            curr = curr.next;
+
         }
 
-        return resHead;
-
-    }
-
-    private void mergeNode (ListNode listNode1, ListNode listNode2, ListNode resNode)
-    {
-        if (listNode1 == null && listNode2 == null)
-        {
-            return;
-        }
-        else if (listNode1 == null)
-        {
-            resNode.next = new ListNode(listNode2.val);
-            mergeNode(listNode1, listNode2.next, resNode.next);
-        }
-        else if (listNode2 == null)
-        {
-            resNode.next = new ListNode(listNode1.val);
-            mergeNode(listNode1.next, listNode2, resNode.next);
-        }
-        else if (listNode1.val < listNode2.val)
-        {
-            resNode.next = new ListNode(listNode1.val);
-            mergeNode(listNode1.next, listNode2, resNode.next);
-        }
-        else
-        {
-            resNode.next = new ListNode(listNode2.val);
-            mergeNode(listNode1, listNode2.next, resNode.next);
-        }
-
+        return fakeHead.next;    
     }
 }
