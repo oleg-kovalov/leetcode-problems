844ce36024be6e3a3971b8058ef8ed7de50007e4
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-04-29 10:27:59 +0200

    Time: 2 ms (85.71%), Space: 48 MB (81.92%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.java
+++ 0143-reorder-list/0143-reorder-list.java
@@ -8,24 +8,51 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
-
-//    1 - 4 - 2 - 3
-//            ^   ^
-
-
-//     1 - 5 - 2 - 3 - 4
-//                 ^   ^
-//     4  
-//     3
-//     2
-//     5
-//     1 
-//     
-
-
 class Solution {
     public void reorderList(ListNode head) {
-        return;
+        if (head.next == null) return;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        ListNode middle = slow.next;
+        ListNode tail = fast.next != null ? fast.next : fast;
+
+        System.out.println(middle.val);
+        System.out.println(tail.val);
+
+
+        ListNode curr = middle;
+        while (curr != tail)
+        {
+            ListNode tmpNext = curr.next;
+            curr.next = tail.next;
+            tail.next = curr;
+            curr = tmpNext;
+        }
+
+        ListNode first = head;
+        ListNode second = tail;
+        while (second != null)
+        {
+            ListNode tmpFirst = first.next; 
+            ListNode tmpSecond = second.next;
+
+            first.next = second;
+            second.next = tmpFirst;
+            first = tmpFirst;
+            second = tmpSecond;
+
+        }
+
+        first.next = null;
+
+
     }
 }
