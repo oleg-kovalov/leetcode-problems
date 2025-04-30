dffbf10428600332d5c0398763e680790fc0c487
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-04-30 16:26:25 +0200

    Time: 0 ms (100%), Space: 44.9 MB (11.87%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -16,53 +16,37 @@ class Node {
 class Solution {
     public Node copyRandomList(Node head) {
         if (head == null) return null;
-
+    
         Node dummy = new Node(-1);
         dummy.next = head;
 
         Node dummy2 = new Node(-1);
 
+        HashMap<Node, Node> nodes = new HashMap<>();
+
         Node curr = dummy;
         Node curr2 = dummy2;
-        while(curr != null)
+        while (curr != null)
         {
+            if (curr.next == null)
+            {
+                curr2.next = null;
+            } else {
+                curr2.next = new Node(curr.next.val);
+                nodes.put(curr.next, curr2.next);
+            }
             curr = curr.next;
-            curr2.next = (curr == null ? null : new Node(curr.val));
             curr2 = curr2.next;
         }
 
-        Node source1 = dummy;
-        Node source2 = dummy2;
-
-        while (source1.next != null)
+        curr = dummy.next;
+        while (curr != null)
         {
-            source1 = source1.next;
-            source2 = source2.next;
-
-            Node target = source1.random;
-            if (target == null) 
-            {
-                source2.random = null;
-            } else {
-                Node target1 = dummy;
-                Node target2 = dummy2;
-                while (target1 != target)
-                {
-                    target1 = target1.next;
-                    target2 = target2.next;
-                }
-                source2.random = target2;
-            }
+            Node target = curr.random;
+            nodes.get(curr).random = nodes.get(target);
+            curr = curr.next;
         }
-        
 
-
-        // Node print = dummy2;
-        // while (print != null)
-        // {
-        //     print = print.next;
-        //     System.out.print(print == null ? null : print.val + " -> ");
-        // }
 
         return dummy2.next;
 
