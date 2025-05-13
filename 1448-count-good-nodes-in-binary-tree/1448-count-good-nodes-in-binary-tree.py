9a22e30ff9a026987689be9a9b252cdca7697084
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-05-13 16:11:09 +0200

    Time: 152 ms (32.48%), Space: 32 MB (81.02%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -5,13 +5,38 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+
     def goodNodes(self, root: TreeNode) -> int:
-        return self.dfs(root, root.val)
+        count = 0
+        queue = deque()
+        queue.append((root, root.val))
+
+        while len(queue) > 0:
+            pair = queue.popleft()
+
+            if pair[0].val >= pair[1]:
+                count += 1
+
+            newMax = max(pair[0].val, pair[1])
+
+            if pair[0].left: 
+                queue.append((pair[0].left, newMax))
+            if pair[0].right: 
+                queue.append((pair[0].right, newMax))
 
 
-    def dfs(self, root: TreeNode, path: int) -> int:
-        if not root: return 0
-        newPath = max(path, root.val)
+        return count
 
-        return self.dfs(root.left, newPath) + self.dfs(root.right, newPath) + (1 if root.val >= path else 0) 
+
+    # DFS
+
+    # def goodNodes(self, root: TreeNode) -> int:
+    #     return self.dfs(root, root.val)
+
+
+    # def dfs(self, root: TreeNode, path: int) -> int:
+    #     if not root: return 0
+    #     newPath = max(path, root.val)
+
+    #     return self.dfs(root.left, newPath) + self.dfs(root.right, newPath) + (1 if root.val >= path else 0) 
         
