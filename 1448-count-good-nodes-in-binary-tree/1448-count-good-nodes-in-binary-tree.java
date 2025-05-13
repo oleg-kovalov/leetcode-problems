e836bfad538c72555f584bddbaffe52cc1eb0443
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-05-13 13:55:29 +0200

    Time: 2 ms (85.43%), Space: 54.2 MB (10.82%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -14,31 +14,47 @@
  * }
  */
 class Solution {
-    record NodePair (TreeNode node, int max) {};
+
     public int goodNodes(TreeNode root) {
-
-        int count = 0;
-        LinkedList<NodePair> queue = new LinkedList<>();
-        queue.offer(new NodePair(root, root.val));
-
-        while (queue.size() > 0)
-        {
-            NodePair pair = queue.poll();
-            TreeNode node = pair.node();
-
-            if (node.val >= pair.max())
-            {
-                count += 1;
-            }
-
-            int newMax = Math.max(node.val, pair.max());
-
-            if (node.left != null) queue.offer(new NodePair(node.left, newMax));
-            if (node.right != null) queue.offer(new NodePair(node.right, newMax));
-        }
-
-        return count;
-    
-
+        return dfs(root, root.val);
     }
+
+    private int dfs(TreeNode node, int path)
+    {
+        if (node == null) return 0;
+
+        int newPath = Math.max(path, node.val);
+
+        return dfs(node.left, newPath) 
+            + dfs(node.right, newPath) 
+            + (node.val >= path ? 1 : 0); 
+    }
+
+    // BFS 
+
+    // record NodePair (TreeNode node, int max) {};
+    // public int goodNodes(TreeNode root) {
+
+    //     int count = 0;
+    //     LinkedList<NodePair> queue = new LinkedList<>();
+    //     queue.offer(new NodePair(root, root.val));
+
+    //     while (queue.size() > 0)
+    //     {
+    //         NodePair pair = queue.poll();
+    //         TreeNode node = pair.node();
+
+    //         if (node.val >= pair.max())
+    //         {
+    //             count += 1;
+    //         }
+
+    //         int newMax = Math.max(node.val, pair.max());
+
+    //         if (node.left != null) queue.offer(new NodePair(node.left, newMax));
+    //         if (node.right != null) queue.offer(new NodePair(node.right, newMax));
+    //     }
+
+    //     return count;
+    // }
 }
