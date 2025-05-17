dbde11dcdc6108d58d6b8e59e0c37f9692d809ea
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-05-17 17:42:14 +0200

    Time: 1 ms (13.05%), Space: 42.3 MB (11.8%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,16 +15,35 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        return doubleDfs(root, root);
+        
+        LinkedList<TreeNode[]> stack = new LinkedList<>();
+        stack.push(new TreeNode[] {root, root});
+
+        while (stack.size() > 0)
+        {
+            TreeNode[] nodes = stack.pop();
+            if (nodes[0] == null && nodes[0] == null) continue;
+            if (nodes[0] == null || nodes[1] == null) return false;
+
+            if (nodes[0].val != nodes[1].val) return false;
+            stack.push(new TreeNode[] {nodes[0].left, nodes[1].right});
+            stack.push(new TreeNode[] {nodes[0].right, nodes[1].left});
+        }
+
+        return true;
     }
 
-    private boolean doubleDfs(TreeNode root1, TreeNode root2)
-    {
-        if (root1 == null && root2 == null) return true;
-        if (root1 == null || root2 == null) return false;
+    // public boolean isSymmetric(TreeNode root) {
+    //     return doubleDfs(root, root);
+    // }
 
-        return root1.val == root2.val 
-            && doubleDfs(root1.left, root2.right) 
-            && doubleDfs(root1.right, root2.left); 
-    }
+    // private boolean doubleDfs(TreeNode root1, TreeNode root2)
+    // {
+    //     if (root1 == null && root2 == null) return true;
+    //     if (root1 == null || root2 == null) return false;
+
+    //     return root1.val == root2.val 
+    //         && doubleDfs(root1.left, root2.right) 
+    //         && doubleDfs(root1.right, root2.left); 
+    // }
 }
