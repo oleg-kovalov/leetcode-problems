261b71744de4ab9f573c1299eef88ba359317983
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-05-31 20:36:57 +0200

    Time: 141 ms (62.49%), Space: 41.4 MB (88.92%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,81 +1,57 @@
 class Solution {
-    private String word;
+
+    char[][] board;
+    String word;   
+    int[][] dirs = new int[][] {{-1, 0},{1,0},{0,-1},{0,1}}; 
     boolean[][] visited;
-    boolean found = false;
+
     public boolean exist(char[][] board, String word) {
+        
+        this.board = board;
         this.word = word;
         this.visited = new boolean[board.length][board[0].length];
 
-        char start = word.charAt(0);
-        for (int row=0; row<board.length; row++)
+        for (int row=0; row < board.length; row++)
         {
-            for (int col=0; col<board[row].length; col++)
+            for (int col=0; col < board[row].length; col++)
             {
-                if (board[row][col] - start == 0 && !found)
+                if (board[row][col] == word.charAt(0))
                 {
-                    // System.out.printf("start %s [%s,%s]\n", board[row][col], row, col);
-                    backtrack(col,row,board, 0);
+                    if (backtrack(row, col, 0)) return true;
                 }
             }
         }
 
-        return found;
+        return false;
+
     }
 
-    private void backtrack(int col, int row, char[][] board, int cIdx)
+    private boolean backtrack(int row, int col, int idx)
     {
-        if (found)
+        if (idx == word.length() - 1) return true;
+
+        char nextChar = word.charAt(idx+1);
+
+        visited[row][col] = true;
+        for (int[] dir: dirs)
         {
-            // System.out.println("found");
-            return;
+            int newRow = row + dir[0];
+            int newCol = col + dir[1];
+
+            if (0 <= newRow && newRow < board.length
+             && 0 <= newCol && newCol < board[newRow].length
+             && board[newRow][newCol] == nextChar
+             && !visited[newRow][newCol])
+             {
+                if (backtrack(newRow, newCol, idx+1)) return true;
+             }
         }
 
-        if (col < 0 || col > board[0].length-1)
-        {
-            // System.out.println("out of range");
-            return;
-        }
-        if (row < 0 || row > board.length-1)
-        {
-            // System.out.println("out of range");
-            return;
-        }
+        visited[row][col] = false;
 
-        if (visited[row][col])
-        {
-            // System.out.println("already visited");
-            return;
-        }
-
-        // System.out.println(board[row][col]);
-
-        final char ch = word.charAt(cIdx);
-        if ( board[row][col] - ch != 0)
-        {
-            // System.out.println("backtrack");
-            return;
-        }
-
-        if ( cIdx == word.length()-1)
-        {
-            found = true;
-            // System.out.println("found last char");
-            return;
-        }
-
-        visited[row][col]=true;
-
-        // System.out.println("left");
-        backtrack(col-1, row, board, cIdx+1);   //left
-        // System.out.println("right");
-        backtrack(col+1, row, board, cIdx + 1); //right
-        // System.out.println("up");
-        backtrack(col, row-1, board, cIdx+1);  //up
-        // System.out.println("down");
-        backtrack(col, row+1, board, cIdx+1);  //down
-
-        visited[row][col]=false;
-        // System.out.println("backtrack");
+        return false;
 
     }
+
+
 }
