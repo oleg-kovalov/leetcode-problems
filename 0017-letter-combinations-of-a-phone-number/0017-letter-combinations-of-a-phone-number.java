6df4f7c9d54a196663dda11e5e3c5bdc782e9e58
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-05-31 19:30:54 +0200

    Time: 4 ms (34.35%), Space: 42.2 MB (64.04%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,15 +1,17 @@
 class Solution {
-    HashMap<Integer, char[]> mappings;
+    HashMap<Character, char[]> mappings;
     public List<String> letterCombinations(String digits) {
+        if (digits.length() == 0) return new ArrayList<>();
+        
         mappings = new HashMap<>();
-        mappings.put(2, new char[] {'a','b','c'});
-        mappings.put(3, new char[] {'d','e','f'});
-        mappings.put(4, new char[] {'g','h','i'});
-        mappings.put(5, new char[] {'j','k','l'});
-        mappings.put(6, new char[] {'m','n','o'});
-        mappings.put(7, new char[] {'p','q','r','s'});
-        mappings.put(8, new char[] {'t','u','v'});
-        mappings.put(9, new char[] {'w','x','y','z'});
+        mappings.put('2', new char[] {'a','b','c'});
+        mappings.put('3', new char[] {'d','e','f'});
+        mappings.put('4', new char[] {'g','h','i'});
+        mappings.put('5', new char[] {'j','k','l'});
+        mappings.put('6', new char[] {'m','n','o'});
+        mappings.put('7', new char[] {'p','q','r','s'});
+        mappings.put('8', new char[] {'t','u','v'});
+        mappings.put('9', new char[] {'w','x','y','z'});
         
         List<String> result = new ArrayList<>();
         backtrack(digits, 0, new ArrayList<>(), result);
@@ -30,8 +32,8 @@ class Solution {
             return;
         }
         
-        System.out.println(Integer.parseInt(digits.charAt(idx)));
-        for (char c: mappings.get(Integer.valueOf(digits.charAt(idx))))
+        System.out.println(digits.charAt(idx));
+        for (char c: mappings.get(digits.charAt(idx)))
         {
             currList.add(c);
             backtrack(digits, idx + 1, currList, result);
