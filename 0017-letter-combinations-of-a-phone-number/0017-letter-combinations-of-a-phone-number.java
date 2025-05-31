719245dff97b1fdca619bf14914e977bd5a558ae
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-05-31 19:35:48 +0200

    Time: 0 ms (100%), Space: 42.7 MB (5.28%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,18 +1,19 @@
 class Solution {
-    HashMap<Character, char[]> mappings;
+    char[][] mappings = new char[][] {
+        {},
+        {},
+        {'a','b','c'},
+        {'d','e','f'},
+        {'g','h','i'},
+        {'j','k','l'},
+        {'m','n','o'},
+        {'p','q','r','s'},
+        {'t','u','v'},
+        {'w','x','y','z'}
+    };
     public List<String> letterCombinations(String digits) {
         if (digits.length() == 0) return new ArrayList<>();
-        
-        mappings = new HashMap<>();
-        mappings.put('2', new char[] {'a','b','c'});
-        mappings.put('3', new char[] {'d','e','f'});
-        mappings.put('4', new char[] {'g','h','i'});
-        mappings.put('5', new char[] {'j','k','l'});
-        mappings.put('6', new char[] {'m','n','o'});
-        mappings.put('7', new char[] {'p','q','r','s'});
-        mappings.put('8', new char[] {'t','u','v'});
-        mappings.put('9', new char[] {'w','x','y','z'});
-        
+          
         List<String> result = new ArrayList<>();
         backtrack(digits, 0, new ArrayList<>(), result);
 
@@ -32,8 +33,7 @@ class Solution {
             return;
         }
         
-        System.out.println(digits.charAt(idx));
-        for (char c: mappings.get(digits.charAt(idx)))
+        for (char c: mappings[digits.charAt(idx) - '0'])
         {
             currList.add(c);
             backtrack(digits, idx + 1, currList, result);
