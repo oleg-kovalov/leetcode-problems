9d955f0eb27554ea830623fdd4ab13e388e708d8
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-06-09 17:51:50 +0200

    Time: 108 ms (5.18%), Space: 50.4 MB (17.66%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.java
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.java
@@ -1,28 +1,61 @@
 class Solution {
-    List<Integer> stretched = new ArrayList<>();
-    Random random = new Random();
+    int[] prefix;
     int totalSum = 0;
 
     public Solution(int[] w) {
+        prefix = new int[w.length];
+
+        int runningSum = 0;
         for (int i=0; i<w.length; i++)
         {
-            int weight = w[i];
-            for (int j=0; j<weight; j++)
-            {
-                stretched.add(i);
-            }
-            totalSum += weight;
+            runningSum += w[i];
+            prefix[i] = runningSum;
         }
+
+        totalSum = runningSum;
     }
     
     public int pickIndex() {
-        if (stretched.size() == 1) return stretched.get(0);
+        double num = totalSum * Math.random();
 
-        // int idx = random.nextInt(stretched.size() - 1);
-        int idx = (int) (totalSum * Math.random());
-        return stretched.get(idx);
+        int upperBoundary = 0;
+        while (num > prefix[upperBoundary])
+        {
+            upperBoundary += 1;
+        }
+
+        return upperBoundary;
     }
 }
+
+
+// Brute force
+//
+// class Solution {
+//     List<Integer> stretched = new ArrayList<>();
+//     Random random = new Random();
+//     int totalSum = 0;
+
+//     public Solution(int[] w) {
+//         for (int i=0; i<w.length; i++)
+//         {
+//             int weight = w[i];
+//             for (int j=0; j<weight; j++)
+//             {
+//                 stretched.add(i);
+//             }
+//             totalSum += weight;
+//         }
+//     }
+    
+//     public int pickIndex() {
+//         if (stretched.size() == 1) return stretched.get(0);
+
+//         // int idx = random.nextInt(stretched.size() - 1);
+//         int idx = (int) (totalSum * Math.random());
+//         return stretched.get(idx);
+//     }
+// }
 
 /**
  * Your Solution object will be instantiated and called as such:
