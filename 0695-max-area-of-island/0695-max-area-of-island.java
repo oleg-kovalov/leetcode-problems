f6b9a637dee9fd279639a063ee416c31fa8bce7e
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-06-10 17:36:53 +0200

    Time: 3 ms (31.74%), Space: 45 MB (25.33%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int[][] dirs = new int[][] {{1,0},{-1,0},{0,1},{0,-1}};
+
+        boolean[][] visited = new boolean[rows][cols];
+        int maxArea = 0;
+        
+        for (int row=0; row<rows; row++)
+        {
+            for (int col=0; col<cols; col++)
+            {
+                if (grid[row][col] != 1) continue;
+                if (visited[row][col]) continue;
+
+                LinkedList<int[]> queue = new LinkedList<>();
+                queue.offer(new int[] {row, col});
+                visited[row][col] = true;
+                int currArea = 1;
+
+                while (queue.size() > 0)
+                {
+                    int[] cell = queue.poll();
+
+                    for (int[] dir: dirs)
+                    {
+                        int newRow = cell[0] + dir[0];
+                        int newCol = cell[1] + dir[1];
+
+                        if (0 <= newRow && newRow < rows
+                            && 0 <= newCol && newCol < cols
+                            && grid[newRow][newCol] == 1
+                            && !visited[newRow][newCol])
+                        {
+                            queue.offer(new int[] {newRow, newCol});
+                            visited[newRow][newCol] = true;
+                            currArea += 1;
+                        }
+                    }
+                }
+
+                maxArea = Math.max(maxArea, currArea);
+            }
+        }        
+
+        return maxArea;
+    }
+}
