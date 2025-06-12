40412089464d163007605b36cbb27d5fb34b02b2
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-06-12 19:22:50 +0200

    Time: 7 ms (91.41%), Space: 47.9 MB (72.88%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.java
+++ 0286-walls-and-gates/0286-walls-and-gates.java
@@ -4,41 +4,44 @@ class Solution {
         int cols = rooms[0].length;
         final int WALL = -1;
         final int GATE = 0;
+        final int INF = Integer.MAX_VALUE;
         int[][] dirs = new int[][] {{1,0},{-1,0},{0,1},{0,-1}}; 
 
+        Queue<int[]> queue = new LinkedList<>();
         for (int row=0; row<rows; row++)
         {
             for (int col=0; col<cols; col++)
             {
                 if (rooms[row][col] == GATE)
+                    queue.offer(new int[] {row, col});
+            }
+        }
+
+        int distance = 0;
+        int size = queue.size();
+        while (queue.size() > 0)
+        {
+            int[] cell = queue.poll();
+
+            for (int[] dir: dirs)
+            {
+                int newRow = cell[0] + dir[0];
+                int newCol = cell[1] + dir[1];
+
+                if (0 <= newRow && newRow < rows
+                    && 0 <= newCol && newCol < cols
+                    && rooms[newRow][newCol] == INF)
                 {
-                    LinkedList<int[]> queue = new LinkedList<>();
-                    queue.offer(new int[] {row, col, 1});
-
-                    boolean[][] visited = new boolean[rows][cols];
-
-                    while (queue.size() > 0)
-                    {
-                        int[] cell = queue.poll();
-                        int dist = cell[2];
-
-                        for (int[] dir: dirs)
-                        {
-                            int newRow = cell[0] + dir[0];
-                            int newCol = cell[1] + dir[1];
-
-                            if (0 <= newRow && newRow < rows
-                                && 0 <= newCol && newCol < cols
-                                && rooms[newRow][newCol] > 0 
-                                && !visited[newRow][newCol])
-                            {
-                                visited[newRow][newCol] = true;
-                                rooms[newRow][newCol] = Math.min(dist, rooms[newRow][newCol]);
-                                queue.offer(new int[] {newRow, newCol, dist + 1});
-                            }
-                        }
-                    }                    
+                    rooms[newRow][newCol] = distance + 1;
+                    queue.offer(new int[] {newRow, newCol});    
                 }
+            }
+
+            size -= 1;
+            if (size == 0)
+            {
+                size = queue.size();
+                distance += 1;
             }
         }
 
