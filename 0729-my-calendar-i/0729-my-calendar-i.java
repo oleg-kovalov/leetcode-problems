cc7e97f8cfcd86e39dd228bd0f8667cb6fbafb14
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-06-17 17:59:58 +0200

    Time: 23 ms (50.47%), Space: 46.1 MB (12.01%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.java
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -1,34 +1,91 @@
 class MyCalendar {
 
-    ArrayList<int[]> events = new ArrayList<>();
+    STN root;
 
     public MyCalendar() {
-        
+        root = new STN(0, 1000_000_000, false);
     }
     
     public boolean book(int startTime, int endTime) {
-        // intersection would be if condition holds
-        // existingStart < newEnd && newStart < existingEnd
-        int leftId = -1;
-        int lo = 0;
-        int hi = events.size() - 1;
-        while (lo <= hi)
+        if (conflict(startTime, endTime - 1, root))
         {
-            int mid = lo + (hi - lo) / 2;
-            if (events.get(mid)[0] < endTime)
-            {
-                leftId = mid;
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;
-            }
+            return false;
+        } else {
+            insert(startTime, endTime - 1, root, true);
+            return true;
         }
-        if (leftId != -1 && startTime < events.get(leftId)[1]) return false;
-
-        events.add(leftId+1, new int[] {startTime, endTime});
-        return true;
     }
 
+    private boolean conflict(int left, int right, STN node)
+    {
+        if (node.leftChild == null) return node.val;
+
+        if (node.left == left && node.right == right)
+        {
+            return true;
+        }
+
+        int mid = node.left + (node.right - node.left) / 2;
+        if (right <= mid)
+        {
+            return conflict(left, right, node.leftChild);
+        } else if (left >= mid+1)
+        {
+            return conflict(left, right, node.rightChild);
+        } else {
+            return conflict(left, mid, node.leftChild)
+                || conflict(mid+1, right, node.rightChild);
+        }
+
+    }
+
+    private void insert(int left, int right, STN node, boolean val)
+    {
+        if (node.left == left && node.right == right)
+        {
+            node.val = val;
+            node.leftChild = node.rightChild = null;
+            return;
+        }
+
+        int mid = node.left + (node.right - node.left) / 2;
+
+        if (node.leftChild == null)
+        {
+            node.leftChild = new STN(node.left, mid, node.val);
+            node.rightChild = new STN(mid+1, node.right, node.val);
+        }
+
+        if (right <= mid)
+        {
+            insert(left, right, node.leftChild, val);
+        } else if (left >= mid+1)
+        {
+            insert(left, right, node.rightChild, val);
+        } else {
+            insert(left, mid, node.leftChild, val);
+            insert(mid+1, right, node.rightChild, val);
+        }
+
+        node.val = node.leftChild.val && node.rightChild.val;
+
+    }
+
+    class STN {
+        STN leftChild;
+        STN rightChild;
+
+        int left;
+        int right;
+        boolean val;
+
+        STN(int left, int right, boolean val)
+        {
+            this.left = left;
+            this.right = right;
+            this.val = val;
+        }
+    }
 }
 
 /**
