8d79a49d57581205f1c95f96f941e137feeeb32d
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-06-19 19:15:53 +0200

    Time: 56 ms (90.32%), Space: 55.4 MB (8.96%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.java
+++ 0127-word-ladder/0127-word-ladder.java
@@ -1,47 +1,15 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         
-        Map<String, List<String>> adjMap = new HashMap<>();
+        Map<String, List<String>> adjMap = new HashMap<>(); // D*g => [Dog, Dig]
         for (int i=0; i<wordList.size(); i++)
         {
-            for (int j=i+1; j<wordList.size(); j++)
+            String word = wordList.get(i);
+            for (int j=0; j<word.length(); j++)
             {
-                String word1 = wordList.get(i);
-                String word2 = wordList.get(j);
-                int mismatch = 0;
-                for (int k=0; k<word1.length(); k++)
-                {
-                    if (word1.charAt(k) != word2.charAt(k))
-                    {
-                        mismatch += 1;
-                        if (mismatch > 1) break;
-                    }
-                }
-                if (mismatch == 1)
-                {
-                    adjMap.putIfAbsent(word1, new ArrayList<>());
-                    adjMap.get(word1).add(word2);
-                    adjMap.putIfAbsent(word2, new ArrayList<>());
-                    adjMap.get(word2).add(word1);
-                }
-            }
-        }
-
-        for (String word: wordList)
-        {
-            int mismatch = 0;
-            for (int k=0; k<beginWord.length(); k++)
-            {
-                if (beginWord.charAt(k) != word.charAt(k))
-                {
-                    mismatch += 1;
-                    if (mismatch > 1) break;
-                }
-            }
-            if (mismatch == 1)
-            {
-                adjMap.putIfAbsent(beginWord, new ArrayList<>());
-                adjMap.get(beginWord).add(word);
+                String partial = word.substring(0, j) + "*" + word.substring(j+1, word.length());
+                adjMap.putIfAbsent(partial, new ArrayList<>());
+                adjMap.get(partial).add(word);
             }
         }
 
@@ -52,7 +20,6 @@ class Solution {
 
         LinkedList<String> queue = new LinkedList<>();
         queue.offer(beginWord);
-        visited.add(beginWord);
         int level = 1;
         int size = queue.size();
 
@@ -63,12 +30,16 @@ class Solution {
 
             if (word.equals(endWord)) return level;
 
-            for (String nextWord: adjMap.getOrDefault(word, Collections.emptyList()))
+            for (int i=0; i<word.length(); i++)
             {
+                String partial = word.substring(0, i) + "*" + word.substring(i+1, word.length());
+                for (String nextWord: adjMap.getOrDefault(partial, Collections.emptyList()))
+                {
                 if (visited.contains(nextWord)) continue;
 
                 visited.add(nextWord);
                 queue.offer(nextWord);
+                }
             }
 
             if (size == 0)
