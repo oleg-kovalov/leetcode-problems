23a21f135184c0dfdb0178e238571777831c77ef
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-06-22 17:21:35 +0200

    Time: 98 ms (39.05%), Space: 46.4 MB (13.49%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,39 +1,46 @@
 class Solution {
     public String longestPalindrome(String s) {
+        int n = s.length();
+        int resultStart = 0;
+        int resultEnd = 0;
 
-        String result = "";
-
-        for (int i=0; i<s.length(); i++)
+        boolean[][] dp = new boolean[n][n]; // boolean true if it's palindrone at start, end indices 
+        int len = 1;
+        for (int start=0; start + len-1 < n; start++)
         {
-            // palindrome of odd length
-            int l=i;
-            int r=i;
+            dp[start][start + len-1] = true; // size 1 is always a palindrome
+        }
 
-            while (0 <= l && r < s.length() && s.charAt(l) == s.charAt(r))
+        len = 2;
+        for (int start=0; start + len-1 < n; start++)
+        {
+            dp[start][start + len-1] = (s.charAt(start) == s.charAt(start + len-1)); // size 2
+            if (dp[start][start + len-1])
             {
-                if (r - l + 1 > result.length())
-                {
-                    result = s.substring(l, r+1);
-                }
-                l -= 1;
-                r += 1;
-            }
-
-            // palindrome of even length
-            l=i;
-            r=i+1;
-
-            while (0 <= l && r < s.length() && s.charAt(l) == s.charAt(r))
-            {
-                if (r - l + 1 > result.length())
-                {
-                    result = s.substring(l, r+1);
-                }
-                l -= 1;
-                r += 1;
+                resultStart = start;
+                resultEnd = start + len - 1;
             }
         }
 
-        return result;
+        for (len=3; len < n + 1; len++)
+        {
+            for (int start=0; start + len-1<n; start++)
+            {
+                dp[start][start + len-1] = dp[start + 1][start+len-1 - 1] && (s.charAt(start) == s.charAt(start + len-1));          
+                if (dp[start][start + len-1])
+                {
+                    resultStart = start;
+                    resultEnd = start + len - 1;
+                }
+            }
+        }
+
+        return s.substring(resultStart, resultEnd + 1);
+
+
+
+
+
+
     }
 }
