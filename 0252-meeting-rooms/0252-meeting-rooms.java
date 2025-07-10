8c10694f29f2f798e9481cacd444d611b93be7c1
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-07-10 12:18:51 +0200

    Time: 3 ms (99.78%), Space: 45.8 MB (94.79%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.java
+++ 0252-meeting-rooms/0252-meeting-rooms.java
@@ -1,15 +1,25 @@
 class Solution {
-
     public boolean canAttendMeetings(int[][] intervals) {
-
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         
-        for (int i=0; i < intervals.length - 1; i++)
+        int maxEnd = 0;
+        for (int[] interval: intervals) 
+            maxEnd = Math.max(maxEnd, interval[1]);
+
+        int[] line = new int[maxEnd + 1];
+        for (int[] interval: intervals)
         {
-            if (intervals[i][1] > intervals[i+1][0]) 
-                return false;
+            line[interval[0]] += 1;
+            line[interval[1]] -= 1;
         }
         
+        int count = 0;
+        for (int i=0; i<line.length; i++)
+        {
+            count += line[i];
+            if (count > 1) return false;
+        } 
+
         return true;
+
     }
 }
