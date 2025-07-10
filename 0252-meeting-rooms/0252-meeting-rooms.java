456c242079b1e6a1a97418482048f561a413b400
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-07-10 12:47:50 +0200

    Time: 7 ms (24.97%), Space: 45.7 MB (97.51%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.java
+++ 0252-meeting-rooms/0252-meeting-rooms.java
@@ -1,25 +1,39 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        
-        int maxEnd = 0;
-        for (int[] interval: intervals) 
-            maxEnd = Math.max(maxEnd, interval[1]);
+    
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
 
-        int[] line = new int[maxEnd + 1];
-        for (int[] interval: intervals)
+        for (int i=1; i<intervals.length; i++)
         {
-            line[interval[0]] += 1;
-            line[interval[1]] -= 1;
+            if (intervals[i][0] < intervals[i-1][1]) return false;
         }
-        
-        int count = 0;
-        for (int i=0; i<line.length; i++)
-        {
-            count += line[i];
-            if (count > 1) return false;
-        } 
 
-        return true;
-
+        return true; 
     }
+
+    // Line Sweep
+    
+    // public boolean canAttendMeetings(int[][] intervals) {
+        
+    //     int maxEnd = 0;
+    //     for (int[] interval: intervals) 
+    //         maxEnd = Math.max(maxEnd, interval[1]);
+
+    //     int[] line = new int[maxEnd + 1];
+    //     for (int[] interval: intervals)
+    //     {
+    //         line[interval[0]] += 1;
+    //         line[interval[1]] -= 1;
+    //     }
+        
+    //     int count = 0;
+    //     for (int i=0; i<line.length; i++)
+    //     {
+    //         count += line[i];
+    //         if (count > 1) return false;
+    //     } 
+
+    //     return true;
+
+    // }
 }
