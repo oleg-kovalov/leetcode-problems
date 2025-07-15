e67383a7dd5d3de9575f69d6b5fd7bb044507498
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-07-15 21:33:57 +0200

    Time: 64 ms (13.37%), Space: 45.5 MB (92.95%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -7,10 +7,12 @@ class Solution {
         for (int i=0; i<nums.length; i++)
         {
             int num = nums[i];
+            if (!dp[i]) continue; // this cell not reacheable
+
             for (int j=i+1; j<i+num+1; j++)
             {
-                if (j == nums.length) break;
-                dp[j] = dp[i];
+                if (j == nums.length) return true; // already enough jump
+                dp[j] = true;
             }
         }
 
