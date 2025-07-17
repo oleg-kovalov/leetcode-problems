7a7325a6c093887933294da79026fb094421c141
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-07-17 19:54:07 +0200

    Time: 6 ms (58.69%), Space: 42.2 MB (60.57%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.java
+++ 0763-partition-labels/0763-partition-labels.java
@@ -18,21 +18,12 @@ class Solution {
 
         List<Integer> result = new ArrayList<>();
 
-        // start idx, letter
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> Integer.compare(a[0], b[0]));
-        for (int i=0; i<starts.length; i++)
-        {
-            if (starts[i] == -1) continue;
-            
-            minHeap.offer(new int[] {starts[i], i});
-        }       
-
         int offset = -1;
         int partitionEnd = -1;
-        while (minHeap.size() > 0)
+        for (int i=0; i<s.length(); i++)
         {
-            int[] entry = minHeap.poll();
-            int start = entry[0], letter = entry[1];
+            int letter = s.charAt(i) - 'a';
+            int start = starts[letter];
 
             if (partitionEnd != -1 && start > partitionEnd)
             {
@@ -42,6 +33,7 @@ class Solution {
             int end = ends[letter];
             partitionEnd = Math.max(partitionEnd, end);
         }
+
 
         result.add(partitionEnd - offset);
 
