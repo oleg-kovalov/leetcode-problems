37fee1c2e516a35681de558222182ca1659e0840
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-07-20 13:28:36 +0200

    Time: 1 ms (33.45%), Space: 42.7 MB (5.12%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,128 +1,32 @@
 class Solution {
+    Boolean[][] visited;
     public boolean checkValidString(String s) {
-    
-        return checkForwards(s) && checkBackwards(s);
-        // return checkForwards(s);
-        // return checkBackwards(s);
-    }
-
-    public boolean checkForwards(String s) {
-
-        int open = 0;
-        int bracketStart = -1;
-        for (int i=0; i<s.length(); i++)
-        {
-            if (s.charAt(i) != '*') 
-            {
-                bracketStart = i;
-                break;
-            }
-            open += 1;
-        }
-
-        int close = 0;
-        int bracketEnd = -1;
-        for (int i=s.length()-1; i>=0; i--)
-        {
-            if (s.charAt(i) != '*') {
-                bracketEnd = i;
-                break;
-            }
-            close += 1;
-        }
-
-        int openClose = 0;
-
-        if (bracketStart == -1) return true;
-
-        int balance = 0; // open +, close -
-        for (int i=bracketStart; i<bracketEnd + 1; i++)
-        {
-            char c = s.charAt(i);
-            if (c == '(')
-            {
-                balance += 1;
-            } else if (c == ')')
-            {
-                balance -= 1;
-                if (balance < 0) {
-                    if (open > 0) {
-                        open -= 1;
-                        balance += 1;
-                    }
-                    else if (openClose > 0) 
-                    {
-                        openClose -= 1;
-                        balance += 1;
-                    }
-                    else return false;
-                }
-            } else {
-                openClose += 1;
-            }
-        }
-
-        return balance <= close + openClose; 
+        
+        visited = new Boolean[s.length()][s.length()];
+        return backtrack(0, 0, s);
 
     }
 
-    public boolean checkBackwards(String s) {
+    private boolean backtrack(int idx, int balance, String s)
+    {
+        if (idx == s.length()) return balance == 0;
 
-        int open = 0;
-        int bracketStart = -1;
-        for (int i=0; i<s.length(); i++)
+        if (balance < 0) return false; // more close brackets than open
+
+        if (visited[idx][balance] != null) return visited[idx][balance];
+
+        char c = s.charAt(idx);
+        if (c == '(')
         {
-            if (s.charAt(i) != '*') 
-            {
-                bracketStart = i;
-                break;
-            }
-            open += 1;
-        }
-
-        int close = 0;
-        int bracketEnd = -1;
-        for (int i=s.length()-1; i>=0; i--)
+            return visited[idx][balance] = backtrack(idx + 1 , balance + 1, s);
+        } else if (c == ')')
         {
-            if (s.charAt(i) != '*') {
-                bracketEnd = i;
-                break;
-            }
-            close += 1;
+            return visited[idx][balance] = backtrack(idx + 1, balance - 1, s);
+        } else {
+            return visited[idx][balance] = 
+            backtrack(idx + 1, balance, s) //empty
+            || backtrack(idx + 1, balance + 1, s) //open
+            || backtrack(idx + 1, balance - 1, s); // close 
         }
-
-        int openClose = 0;
-
-        if (bracketEnd == -1) return true;
-
-        int balance = 0; // close +, open -
-        for (int i=bracketEnd; i>=bracketStart; i--)
-        {
-            char c = s.charAt(i);
-            if (c == ')')
-            {
-                balance += 1;
-            } else if (c == '(')
-            {
-                balance -= 1;
-                if (balance < 0) {
-                    if (close > 0) {
-                        close -= 1;
-                        balance += 1;
-                    }
-                    else if (openClose > 0) 
-                    {
-                        openClose -= 1;
-                        balance += 1;
-                    }
-                    else return false;
-                }
-            } else {
-                openClose += 1;
-            }
-        }
-
-        return balance <= open + openClose; 
-
     }
 }
