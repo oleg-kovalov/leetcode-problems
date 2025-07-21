e45b5639cc9e0d6fb0ed3368512a8d017d73907c
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-07-21 13:59:01 +0200

    Time: 15 ms (40.16%), Space: 50.4 MB (5.3%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,42 +1,80 @@
 class Solution {
+
     public int networkDelayTime(int[][] times, int n, int k) {
+    
+        int[] costs = new int[n + 1]; // 1 - based idx
+        Arrays.fill(costs, Integer.MAX_VALUE);
+        costs[k] = 0;
 
-        Map<Integer, List<int[]>> adjMap = new HashMap<>();
-        for (int[] time: times)
+        for (int i=0; i<n; i++)
         {
-            adjMap.putIfAbsent(time[0], new ArrayList<>());
-            adjMap.get(time[0]).add(new int[] {time[1], time[2]});
-        }
-
-        Map<Integer, Integer> cache = new HashMap<>(); // node -> time
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> Integer.compare(a[1],b[1])); // node, time
-        minHeap.offer(new int[] {k, 0});
-        cache.put(k, 0);
-
-        Set<Integer> seen = new HashSet<>();
-        seen.add(k);
-        int minTime = -1;
-
-        while (minHeap.size() > 0)
-        {
-            int[] entry = minHeap.poll();
-            int node = entry[0], time = entry[1];
-
-            seen.add(node);
-            minTime = Math.max(minTime, cache.get(node));
-            if (seen.size() == n) return minTime;
-
-            for (int[] next: adjMap.getOrDefault(node, new ArrayList<>()))
+            int[] nextCosts = Arrays.copyOf(costs, costs.length);
+            for (int[] edge: times)
             {
-                int nextNode = next[0], nextTime = time + next[1];
+                int src=edge[0], dest=edge[1], weight=edge[2];
+                if (costs[src] == Integer.MAX_VALUE) continue;
 
-                if (cache.containsKey(nextNode) && cache.get(nextNode) <= nextTime) continue;
-
-                minHeap.offer(new int[] {nextNode, nextTime});
-                cache.put(nextNode, nextTime);
+                int nextCost = costs[src] + weight;
+                nextCosts[dest] = Math.min(nextCosts[dest], nextCost);
             }
+
+            costs = nextCosts; 
+            // System.out.println(Arrays.toString(costs));
+
         }
 
-        return -1;
+        int result = -1;
+        for (int i=1; i<costs.length; i++)
+        {
+            if (costs[i] == Integer.MAX_VALUE) return -1; // node was unreachable
+            result = Math.max(result, costs[i]);
+        }
+
+        return result;
     }
 }
+
+
+// DIJKSTRA
+
+//     public int networkDelayTime(int[][] times, int n, int k) {
+
+//         Map<Integer, List<int[]>> adjMap = new HashMap<>();
+//         for (int[] time: times)
+//         {
+//             adjMap.putIfAbsent(time[0], new ArrayList<>());
+//             adjMap.get(time[0]).add(new int[] {time[1], time[2]});
+//         }
+
+//         Map<Integer, Integer> cache = new HashMap<>(); // node -> time
+//         PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> Integer.compare(a[1],b[1])); // node, time
+//         minHeap.offer(new int[] {k, 0});
+//         cache.put(k, 0);
+
+//         Set<Integer> seen = new HashSet<>();
+//         seen.add(k);
+//         int minTime = -1;
+
+//         while (minHeap.size() > 0)
+//         {
+//             int[] entry = minHeap.poll();
+//             int node = entry[0], time = entry[1];
+
+//             seen.add(node);
+//             minTime = Math.max(minTime, cache.get(node));
+//             if (seen.size() == n) return minTime;
+
+//             for (int[] next: adjMap.getOrDefault(node, new ArrayList<>()))
+//             {
+//                 int nextNode = next[0], nextTime = time + next[1];
+
+//                 if (cache.containsKey(nextNode) && cache.get(nextNode) <= nextTime) continue;
+
+//                 minHeap.offer(new int[] {nextNode, nextTime});
+//                 cache.put(nextNode, nextTime);
+//             }
+//         }
+
+//         return -1;
+//     }
+// }
