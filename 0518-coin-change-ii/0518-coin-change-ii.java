fac39f3cfbf23993f1494731f778841cf9255fad
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-08-05 13:41:52 -0400

    Time: 15 ms (19.31%), Space: 51.7 MB (7.54%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,28 +1,59 @@
 class Solution {
-    int[][] memo;
+
     public int change(int amount, int[] coins) {
         
-        memo = new int[amount + 1][coins.length + 1];
-        for (int i=0; i<memo.length; i++)
+        int[][] dp = new int[amount + 1][coins.length];
+        for (int j=0; j<dp[0].length; j++)
         {
-            Arrays.fill(memo[i], -1);
+            dp[0][j] = 1;
         }
 
-        return dfs(amount, 0, coins);
+        for (int i=1; i<dp.length; i++)
+        {
+            for (int j=0; j<dp[0].length; j++)
+            {
+                dp[i][j] = 
+                        (j == 0 ? 0 : dp[i][j-1]) //skip
+                    +   (i - coins[j] < 0 ? 0 : dp[i - coins[j]][j]); // take
+            }
+        }
+
+        return dp[amount][coins.length - 1];
+    
     }
 
-    private int dfs(int rem, int idx, int[] coins)
-    {
-        if (rem == 0) return 1;
-        if (rem < 0) return 0;
-        if (idx == coins.length) return 0;
 
-        if (memo[rem][idx] != -1) return memo[rem][idx];
 
-        int result =  
-            dfs(rem - coins[idx], idx, coins) // take (and allow to take more later)
-            + dfs(rem, idx + 1, coins); // skip 
 
-        return memo[rem][idx] = result;
-    }
+
+
+
+
+
+    // int[][] memo;
+    // public int change(int amount, int[] coins) {
+        
+    //     memo = new int[amount + 1][coins.length + 1];
+    //     for (int i=0; i<memo.length; i++)
+    //     {
+    //         Arrays.fill(memo[i], -1);
+    //     }
+
+    //     return dfs(amount, 0, coins);
+    // }
+
+    // private int dfs(int rem, int idx, int[] coins)
+    // {
+    //     if (rem == 0) return 1;
+    //     if (rem < 0) return 0;
+    //     if (idx == coins.length) return 0;
+
+    //     if (memo[rem][idx] != -1) return memo[rem][idx];
+
+    //     int result =  
+    //         dfs(rem - coins[idx], idx, coins) // take (and allow to take more later)
+    //         + dfs(rem, idx + 1, coins); // skip 
+
+    //     return memo[rem][idx] = result;
+    // }
 }
