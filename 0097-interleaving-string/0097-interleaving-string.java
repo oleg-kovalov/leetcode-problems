0ce2cf6ebecafd3e0ddbed523b60b27fc8c3297e
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-08-06 12:44:48 -0400

    Time: 7 ms (27.53%), Space: 41.9 MB (45.03%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.java
+++ 0097-interleaving-string/0097-interleaving-string.java
@@ -1,36 +1,83 @@
 class Solution {
 
-    int[][] memo;
     public boolean isInterleave(String s1, String s2, String s3) {
+        if (s1.length() + s2.length() != s3.length()) return false;
+    
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+        dp[0][0] = true;
+
+        for (int len=1; len < s3.length() + 1; len++)
+        {
+            char c3 = s3.charAt(len - 1);
+            for (int i=0; i<len + 1; i++)
+            {
+                int j = len - i;
+                if (i >= dp.length || j >= dp[0].length) continue;
+
+                dp[i][j] = false;
+                if (i > 0)
+                {
+                    dp[i][j] |= 
+                        (c3 == s1.charAt(i-1) && dp[i-1][j]); // try s1
+                }
+                if (j > 0)
+                {
+                    dp[i][j] |= 
+                        (c3 == s2.charAt(j-1) && dp[i][j-1]); // try s2
+                }
+            }
+        }
+
+        // for (int i=0; i<dp.length; i++)
+        //     System.out.println(Arrays.toString(dp[i]));
+
+        return dp[s1.length()][s2.length()];
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // int[][] memo;
+    // public boolean isInterleave(String s1, String s2, String s3) {
         
-        memo = new int[s1.length() + 1][s2.length() + 1]; // 0 - not set, 1 - true, 2 - false
+    //     memo = new int[s1.length() + 1][s2.length() + 1]; // 0 - not set, 1 - true, 2 - false
 
-        return backtrack(0, 0, s1, s2, 0, s3);
+    //     return backtrack(0, 0, s1, s2, 0, s3);
 
-    }
+    // }
 
-    private boolean backtrack(int idx1, int idx2, String s1, String s2, int idx3, String s3)
-    {
-        if (idx1 == s1.length() && idx2 == s2.length() && idx3 == s3.length()) return true;
-        if (idx3 == s3.length()) return false;
+    // private boolean backtrack(int idx1, int idx2, String s1, String s2, int idx3, String s3)
+    // {
+    //     if (idx1 == s1.length() && idx2 == s2.length() && idx3 == s3.length()) return true;
+    //     if (idx3 == s3.length()) return false;
 
-        if (memo[idx1][idx2] != 0) return memo[idx1][idx2]  == 1 ? true : false; 
+    //     if (memo[idx1][idx2] != 0) return memo[idx1][idx2]  == 1 ? true : false; 
 
-        char c3 = s3.charAt(idx3);
+    //     char c3 = s3.charAt(idx3);
 
-        boolean result = false;
-        if (idx1 < s1.length() && s1.charAt(idx1) == c3)
-        {
-            result |= backtrack(idx1 + 1, idx2, s1, s2, idx3 + 1, s3);
-        }
-        if (idx2 < s2.length() && s2.charAt(idx2) == c3)
-        {
-            result |= backtrack(idx1, idx2 + 1, s1, s2, idx3 + 1, s3);
-        }
+    //     boolean result = false;
+    //     if (idx1 < s1.length() && s1.charAt(idx1) == c3)
+    //     {
+    //         result |= backtrack(idx1 + 1, idx2, s1, s2, idx3 + 1, s3);
+    //     }
+    //     if (idx2 < s2.length() && s2.charAt(idx2) == c3)
+    //     {
+    //         result |= backtrack(idx1, idx2 + 1, s1, s2, idx3 + 1, s3);
+    //     }
 
-        memo[idx1][idx2] = (result ? 1 : 2);
+    //     memo[idx1][idx2] = (result ? 1 : 2);
 
-        return result;
+    //     return result;
 
-    }
+    // }
 }
