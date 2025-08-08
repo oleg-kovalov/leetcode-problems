215e27073131d3b78dc146e2fc27da69a4e0d26c
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-08-08 09:55:55 -0400

    Time: 3 ms (93.94%), Space: 45.1 MB (62.79%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,35 +1,39 @@
 class Solution {
     int[][] memo;
     public int minDistance(String word1, String word2) {
-        memo = new int[word1.length()][word2.length()];
+
+        memo = new int[word1.length() + 1][word2.length() + 1];
         for (int i=0; i<word1.length(); i++)
         {
             Arrays.fill(memo[i], -1);
         }
-        
+
         return dfs(0, 0, word1, word2);    
     }
 
-
-    private int dfs(int i1, int i2, String word1, String word2)
+    private int dfs(int idx1, int idx2, String word1, String word2)
     {
-        if (i1 == word1.length()) return word2.length() - i2; // insert all missing
-        if (i2 == word2.length()) return word1.length() - i1; // delete all extra
+        if (idx2 == word2.length()) return word1.length() - idx1;
+        if (idx1 == word1.length()) return word2.length() - idx2;
 
-        if (memo[i1][i2] != -1) return memo[i1][i2];
+        if (memo[idx1][idx2] != -1) return memo[idx1][idx2];
 
-        if (word1.charAt(i1) == word2.charAt(i2))
+        char c1 = word1.charAt(idx1);
+        char c2 = word2.charAt(idx2);
+
+        int result = Integer.MAX_VALUE;
+        if (c1 == c2)
         {
-            return memo[i1][i2] 
-                = dfs(i1 + 1, i2 + 1, word1, word2);
+            result = Math.min(result, dfs(idx1 + 1, idx2 + 1, word1, word2)); //take
         } else {
-            return memo[i1][i2] = 
-                Math.min( 1 + dfs(i1, i2 + 1, word1, word2), // insert
-                    Math.min(
-                             1 + dfs(i1 + 1, i2 + 1, word1, word2), // replace
-                             1 + dfs(i1 + 1, i2, word1, word2) // delete
-                ));
+            result = Math.min(result, 
+                        Math.min(1 + dfs(idx1, idx2 + 1, word1, word2), // insert
+                            Math.min(1 + dfs(idx1 + 1, idx2, word1, word2), // delete
+                                1 + dfs(idx1 + 1, idx2 + 1, word1, word2)))); //replace
         }
+
+        
+        return memo[idx1][idx2] = result;
 
     }
 }
