3233345095532540ecb3a9b144183cbf3ed45a5f
oleg-kovalov <8151031+oleg-kovalov@users.noreply.github.com>
2025-08-18 13:56:20 -0400

    Time: 6 ms (72.82%), Space: 46.2 MB (8.43%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,44 +1,88 @@
 class Solution {
+    
+    int TODO = 0;
+    int IN_PROGRESS = 1;
+    int DONE = 2;
+
     public boolean canFinish(int numCourses, int[][] prerequisites) {
 
         HashMap<Integer, List<Integer>> adjMap = new HashMap<>();
-        HashMap<Integer, Integer> inCount = new HashMap<>();
         for (int[] p: prerequisites)
         {
             adjMap.putIfAbsent(p[1], new ArrayList<>());
             adjMap.get(p[1]).add(p[0]);
 
-            inCount.put(p[0], inCount.getOrDefault(p[0], 0) + 1);
         }
 
-        LinkedList<Integer> queue = new LinkedList<>();   
-        for (int course=0; course < numCourses; course++)
+        int[] visited = new int[numCourses];
+        List<Integer> order = new ArrayList<>();
+
+        for (int i=0; i<numCourses; i++)
         {
-            if (!inCount.containsKey(course))
-            {
-                // root course, no prerequisites
-                queue.offer(course);
-            }
+            if (!dfs(i, order, visited, adjMap)) return false;
         }
 
-        int passedCount = 0;
-        while (queue.size() > 0)
+        return true;
+    }
+
+    private boolean dfs(int course, List<Integer> order, int[] visited, Map<Integer, List<Integer>> adjMap)
+    {
+        if (visited[course] == DONE) return true;
+        if (visited[course] == IN_PROGRESS) return false; //cycle
+
+        visited[course] = IN_PROGRESS;
+
+        for (int nextCourse: adjMap.getOrDefault(course, new ArrayList<>()))
         {
-            int course = queue.poll();  
-            passedCount += 1;
-
-            for (int nextCourse : adjMap.getOrDefault(course, Collections.emptyList()))
-            {
-                inCount.put(nextCourse, inCount.get(nextCourse) - 1);
-                if (inCount.get(nextCourse) == 0)
-                {
-                    // all prerequisites are passed
-                    queue.offer(nextCourse);
-                    inCount.remove(nextCourse);
-                }
-            }
+            if (!dfs(nextCourse, order, visited, adjMap)) return false; //cycle
         }
 
-        return passedCount == numCourses; // if not all courses were passed, there is cycle in the graph
-    }   
+        order.add(course);
+        visited[course] = DONE;
+        return true;
+    }
+
+
+    // public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+    //     HashMap<Integer, List<Integer>> adjMap = new HashMap<>();
+    //     HashMap<Integer, Integer> inCount = new HashMap<>();
+    //     for (int[] p: prerequisites)
+    //     {
+    //         adjMap.putIfAbsent(p[1], new ArrayList<>());
+    //         adjMap.get(p[1]).add(p[0]);
+
+    //         inCount.put(p[0], inCount.getOrDefault(p[0], 0) + 1);
+    //     }
+
+    //     LinkedList<Integer> queue = new LinkedList<>();   
+    //     for (int course=0; course < numCourses; course++)
+    //     {
+    //         if (!inCount.containsKey(course))
+    //         {
+    //             // root course, no prerequisites
+    //             queue.offer(course);
+    //         }
+    //     }
+
+    //     int passedCount = 0;
+    //     while (queue.size() > 0)
+    //     {
+    //         int course = queue.poll();  
+    //         passedCount += 1;
+
+    //         for (int nextCourse : adjMap.getOrDefault(course, Collections.emptyList()))
+    //         {
+    //             inCount.put(nextCourse, inCount.get(nextCourse) - 1);
+    //             if (inCount.get(nextCourse) == 0)
+    //             {
+    //                 // all prerequisites are passed
+    //                 queue.offer(nextCourse);
+    //                 inCount.remove(nextCourse);
+    //             }
+    //         }
+    //     }
+
+    //     return passedCount == numCourses; // if not all courses were passed, there is cycle in the graph
+    // }   
 }
